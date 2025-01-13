a1940152add9c22883493747d714c97c9293409e
TheNeekOfficial <dylan_leslie05@yahoo.com.au>
2025-01-14 09:27:59 +1100

    Removed some unnecessary packages

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = {...}: {
     templates = {
       python = {
         path = ./python;
--- python/flake.nix
+++ python/flake.nix
@@ -5,24 +5,21 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: 
-    let  
-      forAllSystems = nixpkgs.lib.genAttrs ["x86_64-linux" "aarch64-darwin"];
-      pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
-      pythonPackages = (ps: with ps; [
+  outputs = {nixpkgs}: let
+    forAllSystems = nixpkgs.lib.genAttrs ["x86_64-linux" "aarch64-darwin"];
+    pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
+    pythonPackages = ps:
+      with ps; [
         ipython
         pandas
-      ]);
-    in
-    {
-      devShells = forAllSystems (system: {
-        default = pkgs.${system}.mkShellNoCC {
-          packages = with pkgs.${system}; [
-            (python3.withPackages pythonPackages)
-            bash
-            gnugrep
-          ];
-        };
-      });
-    };
+      ];
+  in {
+    devShells = forAllSystems (system: {
+      default = pkgs.${system}.mkShellNoCC {
+        packages = with pkgs.${system}; [
+          (python3.withPackages pythonPackages)
+        ];
+      };
+    });
+  };
 }
