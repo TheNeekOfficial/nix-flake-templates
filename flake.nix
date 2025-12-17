cdd45831d1a0f747073bd0cfad68b7378c975693
TheNeekOfficial <neekspare@proton.me>
2025-12-17 12:43:40 +1100

    made the top level flake actually have the pythonsql output

--- flake.nix
+++ flake.nix
@@ -11,6 +11,10 @@
         path = ./python;
         description = "Basic template for python development w/ pandas";
       };
+      pythonSQL = {
+        path = ./pythonSQL;
+        description = "python devshell with background mysql instance";
+      };
       rust = {
         path = ./rust;
         description = "Basic template for rust development";
